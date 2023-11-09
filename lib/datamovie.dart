bde56b0525c3594cf77b2e68f08a25084a7c81dc
Razka <razka.julian@mhs.itenas.ac.id>
2023-11-09 19:43:34 +0700

    first commit

---
+++ lib/datamovie.dart
@@ -0,0 +1,62 @@
+class Movie {
+  final String name;
+  final String poster;
+  final String banner;
+  final String durasi;
+  final String genre;
+  final String desc;
+
+  Movie({required this.name, required this.poster, required this.banner, required this.durasi, required this.genre, required this.desc});
+}
+
+
+var dataMovie = [
+  Movie(
+      name: "Oppenheimer",
+      poster: "assets/image/poster/oppenp.jpeg",
+      banner: "assets/image/banner/oppenb.jpeg",
+      durasi: "180 minutes",
+      genre: "Historical Drama",
+      desc: "Kisah tentang seorang fisikawan Amerika Serikat bernama J. Robert Oppenheimer yang mengembangkan bom atom."),
+
+  Movie(
+      name: "Fast X",
+      poster: "assets/image/poster/fastxp.jpeg",
+      banner: "assets/image/banner/fastxb.jpeg",
+      durasi: "141 minutes",
+      genre: "Action",
+      desc: "Selama banyak misi dan melawan rintangan yang mustahil, Dom Toretto dan keluarganya telah mengakali dan mengalahkan setiap musuh di jalan mereka. Sekarang, mereka menghadapi lawan paling mematikan yang pernah mereka hadapi: Ancaman mengerikan yang muncul dari bayang-bayang masa lalu yang dipicu oleh dendam berdarah, dan yang bertekad untuk menghancurkan keluarga ini dan menghancurkan segalanyadan semua orangyang dicintai Dom, selamanya. Dalam Fast Five 2011, Dom dan krunya mengalahkan gembong narkoba Brasil Hernan Reyes yang jahat dan memenggal kerajaannya di sebuah jembatan di Rio De Janeiro. Apa yang tidak mereka ketahui adalah bahwa putra Reyes, Dante, menyaksikan semuanya dan telah menghabiskan 12 tahun terakhir mendalangi rencana untuk membuat Dom membayar harga tertinggi. Plot Dante akan menyebarkan keluarga Dom dari Los Angeles ke katakombe Roma, dari Brasil ke London dan dari Portugal ke Antartika. Sekutu baru akan ditempa dan musuh lama akan muncul kembali. Tapi semuanya berubah ketika Dom menemukan bahwa putranya sendiri yang berusia 8 tahun adalah target utama balas dendam Dante."),
+
+  Movie(
+      name: "The Creator",
+      poster: "assets/image/poster/creator.jpg",
+      banner: "assets/image/banner/creator.jpg",
+      durasi: "133 minutes",
+      genre: "Action, Adventure",
+      desc: "Masa depan, di tengah-tengah peperangan umat manusia melawan kekuatan kecederdasan buatan (AI), Joshua, seorang mantan agen khusus militer berpengalaman yang masih berduka atas menghilangnya sang istri, direkrut dalam sebuah misi memburu dan membunuh The Creator, sang perancang AI canggih yang telah mengembangkan sebuah senjata misteri berkekuatan dahsyat dan mampu mengakhiri perang…sekaligus memusnahkan umat manusia. Joshua dan regu elit yang dipimpinnya maju melintasi garis pertahanan musuh, ke pusat teritori gelap yang dikuasai AI, hanya untuk menemukan senjata pemusnah bumi yang harus ia hancurkan ternyata adalah AI yang tampil dalam sosok seorang anak kecil."),
+
+  Movie(
+      name: "Insidious: The Red Door",
+      poster: "assets/image/poster/door.jpg",
+      banner: "assets/image/banner/door.jpg",
+      durasi: "107 minutes",
+      genre: "Horror",
+      desc: "Sepuluh tahun sejak kisah di film kedua, Josh Lambert berangkat ke pesisir timur untuk mengantar putranya, Dalton yang akan mulai kuliah di sebuah universitas bergengsi. Namun, kehidupan kampus yang didambakan Dalton berubah menjadi mimpi buruk ketika makhluk-makhluk jahat dari masa lalunya tiba-tiba datang kembali untuk menghantui. Demi menghentikan teror dan menyingkirkan makhluk-makhluk itu hingga tuntas, Josh dan Dalton harus kembali masuk ke The Further agar mimpi buruk yang menimpa keluarga Lambert dapat benar-benar berakhir."),
+
+  Movie(
+      name: "Interstellar",
+      poster: "assets/image/poster/insp.jpeg",
+      banner: "assets/image/banner/insb.jpeg",
+      durasi: "165 minutes",
+      genre: "Action Sci-fi",
+      desc: "Sebuah tim penjelajah antar galaksi harus melewati lubang cacing dan terjebak di dimensi waktu ruang angkasa dalam upaya untuk menjamin kelangsungan hidup umat manusia di planet bumi."),
+
+  Movie(
+      name: "The Meg",
+      poster: "assets/image/poster/meg.jpg",
+      banner: "assets/image/banner/meg.jpg",
+      durasi: "112 minutes",
+      genre: "Action, Horror, Sci-fi",
+      desc: "Seekor makhluk raksasa menyerang sebuah kapal selam di laut dalam, sehingga para awaknya terjebak di dasar laut Pasifik. Dengan waktu yang semakin menipis, Jonas Taylor, seorang penyelam, harus menyelamatkan para awak kapal, dan juga laut itu sendiri, dari ancaman yang tidak terbayangkan: seekor hiu purba sepanjang 75 kaki, yang dikenal dengan nama Megalodon."),
+
+];
